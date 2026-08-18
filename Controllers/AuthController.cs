f0bddbca82f4c973fdcadc26a62d2458dfd68af9
binay <binay@DESKTOP-3PQIJ7B>
2026-08-18 20:10:30 +0530

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using JWTAuthProject.Models;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+       private readonly IServices _services;
+        public AuthController(IServices services)
+        {
+                _services = services;
+        }
+        [HttpPost("login")]
+        public IActionResult Login(UserDto request)
+        {
+            // Normally validate user from database
+
+            if (request.Username != "admin" ||
+                request.Password != "password")
+            {
+                return Unauthorized(
+                    "Invalid username or password");
+            }
+
+            var token = _services.GenerateToken(
+                1,
+                request.Username,
+                "Admin");
+
+            return Ok(new
+            {
+                token
+            });
+        }
+    }
+}
---
+++ Service/Services.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthProject
+{
+    public class Services: IServices
+    {
+        private readonly IConfiguration _configuration;
+        public Services(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateToken(int userId,string username,string role)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+
+            var claims = new List<Claim>
+        {
+            new Claim(
+                ClaimTypes.NameIdentifier,
+                userId.ToString()),
+
+            new Claim(
+                ClaimTypes.Name,
+                username),
+
+            new Claim(
+                ClaimTypes.Role,
+                role)
+        };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    jwtSettings["Key"]!));
+
+            var credentials =
+                new SigningCredentials(
+                    key,
+                    SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(
+                    Convert.ToDouble(
+                        jwtSettings["ExpiryMinutes"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler()
+                .WriteToken(token);
+        }
+    }
+}
